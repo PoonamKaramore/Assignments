b23c2d781afe8c19b807e7af0ecd43d388117221
PoonamKaramore <88128352+PoonamKaramore@users.noreply.github.com>
2022-02-20 22:27:00 +0530

    Add files via upload

---
+++ Spring Rest/Q1/SpringrestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.springrestAssignment.springrestAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class SpringrestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestAssignmentApplication.class, args);
+	}
+
+}
