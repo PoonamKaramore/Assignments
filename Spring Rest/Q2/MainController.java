4ab957f4d1699361806f5cbfaacc18d73365d496
PoonamKaramore <88128352+PoonamKaramore@users.noreply.github.com>
2022-02-20 22:28:42 +0530

    Add files via upload

---
+++ Spring Rest/Q2/MainController.java
@@ -0,0 +1,23 @@
+package que2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	User user = new User("Poonam","Poonam123");
+
+	@PostMapping("/in")
+	public String login(@RequestBody User user) {
+		System.out.println(user);
+		
+		if(this.user.getUsername().equals(user.getUsername())&& this.user.getPassword().equals(user.getPassword())) {
+		return "Valid User";		
+	}
+	return "Invalid User";
+  }
+}
---
+++ Spring Rest/Q2/Springbootapp.java
@@ -0,0 +1,12 @@
+package que2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootapp {
+
+		public static void main(String[] args) {
+			SpringApplication.run(Springbootapp.class, args);
+		}
+}
